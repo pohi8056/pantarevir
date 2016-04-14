62ad0c18b493e0978595e079c9949ff11de4eec1
drante <anton.kallbom@gmail.com>
2016-04-15 00:18:09 +0200

    segue

--- Pantarevir/LoginViewController.swift
+++ Pantarevir/LoginViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
