c6b518301f677d96a7a75ba13a2d21c2858b2a17
drante <anton.kallbom@gmail.com>
2016-04-18 14:34:23 +0200

    toplista osv

---
+++ Pantarevir/ToplistTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ToplistTableViewCell.swift
+//  Pantarevir
+//
+//  Created by Anton Källbom on 18/04/16.
+//  Copyright © 2016 PonyCorp Inc. All rights reserved.
+//
+
+import UIKit
+
+class ToplistTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
