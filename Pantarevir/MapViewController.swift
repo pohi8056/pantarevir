71b25666bc8154b215f4f90adb728045e9f4640a
drante <anton.kallbom@gmail.com>
2016-05-02 16:53:19 +0200

    fin liten karta

---
+++ Pantarevir/MapViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MapViewController.swift
+//  Pantarevir
+//
+//  Created by Anton Källbom on 02/05/16.
+//  Copyright © 2016 PonyCorp Inc. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
