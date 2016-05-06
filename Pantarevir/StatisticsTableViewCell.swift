e21d7d2370aac04ee3c8ecccc9aa037e9d912dd4
drante <anton.kallbom@gmail.com>
2016-05-06 17:02:46 +0200

    statisticsVC

---
+++ Pantarevir/StatisticsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  StatisticsTableViewCell.swift
+//  Pantarevir
+//
+//  Created by Anton Källbom on 05/05/16.
+//  Copyright © 2016 PonyCorp Inc. All rights reserved.
+//
+
+import UIKit
+
+class StatisticsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
